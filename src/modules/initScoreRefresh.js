82863c9fa9fc60207af920f2ddb5be455455e24e
devendra-alt <devendramulewa171@gmail.com>
2023-07-05 21:15:49 +0530

    Added refresh button method for updating score on leaderboard

---
+++ src/modules/initScoreRefresh.js
@@ -0,0 +1,9 @@
+const refreshBtn = document.querySelector('#refresh-btn');
+
+const initRefreshBtn = (game) => {
+  refreshBtn.addEventListener('click', () => {
+    game.getScores();
+  });
+};
+
+export default initRefreshBtn;
