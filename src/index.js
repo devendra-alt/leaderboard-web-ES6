bafc160337de109a3cd135267fca32bdd07cf773
devendra-alt <devendramulewa171@gmail.com>
2023-07-05 12:09:20 +0530

    Used renderScore(game) function on index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './styles/reset.css';
 import './styles/style.css';
+import Game from './modules/game.js';
+import renderScores from './modules/render';
+
+const game = new Game();
+renderScores(game);
