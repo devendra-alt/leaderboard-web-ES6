79beb8199014307f4952231fe522b0b2b7045fcd
devendra-alt <devendramulewa171@gmail.com>
2023-07-05 21:10:12 +0530

    Added refresh method for updating scores on board

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './styles/reset.css';
 import './styles/style.css';
 import Game from './modules/game.js';
 import initScoreSubmit from './modules/initScoreSubmit.js';
+import initRefreshBtn from './modules/initScoreRefresh';
 
 const game = new Game();
 initScoreSubmit(game);
+initRefreshBtn(game);
