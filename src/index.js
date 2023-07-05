39b229f988a8c733b01aeba326867fb974814071
devendra-alt <devendramulewa171@gmail.com>
2023-07-05 12:16:17 +0530

    Added .js extension for renderScore module import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles/reset.css';
 import './styles/style.css';
 import Game from './modules/game.js';
-import renderScores from './modules/render';
+import renderScores from './modules/render.js';
 
 const game = new Game();
 renderScores(game);
