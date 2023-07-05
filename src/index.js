1669f0f3d73c764a498881271147fd9efb72eef3
devendra-alt <devendramulewa171@gmail.com>
2023-07-05 21:09:27 +0530

    Added score submit method for creating player score

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles/reset.css';
 import './styles/style.css';
 import Game from './modules/game.js';
-import renderScores from './modules/render.js';
+import initScoreSubmit from './modules/initScoreSubmit.js';
 
 const game = new Game();
-renderScores(game);
+initScoreSubmit(game);
