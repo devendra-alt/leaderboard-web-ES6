a0374cdb8a3d453e9867cdf64e916dc6371a20f4
devendra-alt <devendramulewa171@gmail.com>
2023-07-06 18:01:11 +0530

    Added globalMessageActionListner method

--- src/modules/messages.js
+++ src/modules/messages.js
@@ -1,3 +1,13 @@
+import xImg from '../assets/x.png';
+const leaderBoardEl = document.querySelector('#leader-board');
+const globalMessageActionListner = () => {
+  leaderBoardEl.addEventListener('click', (e) => {
+    if (e.target.getAttribute('global-message-exit') === 'exit-msg') {
+      e.target.parentNode.classList.add('hide');
+    }
+  });
+};
+
 export const globalMessageCreater = () => {
   const messageBoxEl = document.createElement('p');
   messageBoxEl.id = 'message-context';
