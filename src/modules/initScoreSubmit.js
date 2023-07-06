39db1d3a0f8262e420ce69088f3598fd8902fcc8
devendra-alt <devendramulewa171@gmail.com>
2023-07-06 10:04:14 +0530

    Added async clickEvent and retriving data from form for POST action

--- src/modules/initScoreSubmit.js
+++ src/modules/initScoreSubmit.js
@@ -2,15 +2,21 @@ const listEl = document.querySelector('#score-btn');
 const userNameField = document.querySelector('#name');
 const scoreField = document.querySelector('#score');
 const initScoreSubmit = (game) => {
-  listEl.addEventListener('click', (e) => {
+  listEl.addEventListener('click', async (e) => {
     e.preventDefault();
     const userName = userNameField.value;
     const score = scoreField.value;
-    game.setScore(userName, score).then((result) => {
-      if (result.status === 201) {
-        game.getScores();
+    userNameField.value = '';
+    scoreField.value = '';
+    try {
+      const respons = await game.setScore(userName, score);
+      const responsData = await respons.json();
+      if (!responsData.message === 'Leaderboard score created correctly.') {
+        throw new Error(responsData.message);
       }
-    });
+    } catch (error) {
+      throw new Error(error);
+    }
   });
 };
 
